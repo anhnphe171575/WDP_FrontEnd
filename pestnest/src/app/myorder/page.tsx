ee9a985a6402492716e9f757f65575b94beb2405
QuynhLD <luongquynhduc4@gmail.com>
2025-07-12 09:33:03 +0700

    chinh sua processing van cho cancel

--- pestnest/src/app/myorder/page.tsx
+++ pestnest/src/app/myorder/page.tsx
@@ -559,9 +559,10 @@ const MyOrderPage = () => {
                                                             onClick={() => handleReturnClick(order)}
                                                             className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
                                                         >
-                                                            Trả hàng
+                                                            Đánh giá
                                                         </button>
-                                                    ) : order.status === 'pending' ? (
+                                                    )
+                                                     : order.status === 'processing' ? (
                                                         <button
                                                             onClick={() => handleCancelClick(order)}
                                                             className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
